aeebf54e09d0f5e875c05f3f2f7d79811bc8d107
ClaraLopez1 <mclopez@mail.austral.edu.ar>
2024-04-02 19:12:11 -0300

    Frontend: ManageRooms (create & delete) + app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,14 @@ import Login from "./Components/Login/Login";
 import TrainerSignup from "./Components/TrainerSignup/TrainerSignup";
 import {useNavigate, Route, Routes} from "react-router-dom";
 import ChangePassword from "./Components/ChangePassword/ChangePassword";
+import AdminHome from "./Components/AdminHome/AdminHome";
+import ManageSchedule from "./Components/ManageSchelude/ManageSchedule";
+import ManageStaff from "./Components/ManageStaff/ManageStaff";
+import ManageRooms from "./Components/ManageRooms/ManageRooms";
+import CreateRoom from "./Components/CreateRoom/CreateRoom";
+import DeleteRoom from "./Components/DeleteRoom/DeleteRoom";
+
+
 function App() {
     const navigate = useNavigate();
   return (
@@ -17,6 +25,15 @@ function App() {
           <Route path="/Login/RestorePassword" element={<ForgotPassword/>}/>
           <Route path="/SignUp/TrainerSignUp" element={<TrainerSignup/>}/>
           <Route path="/Login/RestorePassword/ChangePassword" element={<ChangePassword/>}/>
+          <Route path="/AdminHome" element={<AdminHome/>}/>
+          <Route path="/ManageSchedule" element={<ManageSchedule/>}/>
+          <Route path="/ManageStaff" element={<ManageStaff/>}/>
+          <Route path="/ManageRooms" element={<ManageRooms/>}/>
+          <Route path="/CreateRoom" element={<CreateRoom/>}/>
+          <Route path="/DeleteRoom" element={<DeleteRoom/>}/>
+
+
+
 
       </Routes>
   );
