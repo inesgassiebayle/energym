57ba48bdea983261351444a034c1b77591516d1d
ClaraLopez1 <mclopez@mail.austral.edu.ar>
2024-05-14 11:29:13 -0300

    prof schedule rating

---
+++ src/main/java/org/austral/ing/lab1/dto/LessonIdAvdDto.java
@@ -0,0 +1,43 @@
+package org.austral.ing.lab1.dto;
+
+import java.time.*;
+import java.time.format.*;
+
+public class LessonIdAvdDto {
+  String id;
+  String name;
+  String startDate;
+  String time;
+  String activity;
+  String review;
+
+  public LessonIdAvdDto(String id, String name, String startDate, String time , String activity, String review){
+    this.id = id;
+    this.name = name;
+    this.startDate = startDate;
+    this.time = time;
+    this.activity = activity;
+    this.review = review;
+  }
+
+  public String getName(){
+    return name;
+  }
+
+  public LocalDate getDate(){
+    return LocalDate.parse(startDate, DateTimeFormatter.ISO_LOCAL_DATE);
+  }
+
+  public LocalTime getTime() { return LocalTime.parse(time, DateTimeFormatter.ISO_LOCAL_TIME);
+  }
+  public String getReview() {
+    return review;
+  }
+
+  public String getActivity() {
+    return activity;
+  }
+  public String getId() {
+    return id;
+  }
+}
