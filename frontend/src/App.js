c5b0e6ea7105042c02281172732cdc0ea9cf90fb
ClaraLopez1 <mclopez@mail.austral.edu.ar>
2024-07-06 12:41:55 -0300

    Que se habra directamente en getstarted

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -26,6 +26,7 @@ function App() {
     const navigate = useNavigate();
     return (
         <Routes>
+            <Route path="/" element={<GetStarted/>}/>
             <Route path="/login" element={<Login/>}/>
             <Route path="/signup" element={<SignUp/>}/>
             <Route path="/signup-trainer" element={<TrainerSignup/>}/>
